0b3fce6e34ed50eb82ea1db3f419b9fb9ea03750
Ruslan Martynov <martynovworkmail@gmail.com>
2020-03-20 16:14:33 +0300

    Project created

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Руслан Мартынов on 20.03.2020.
+//  Copyright © 2020 Руслан Мартынов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
