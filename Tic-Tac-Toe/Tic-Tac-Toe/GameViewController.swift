3fd7ed29c22f26087b4ba412bd349b8aba366a12
Ruslan Martynov <martynovworkmail@gmail.com>
2020-03-21 14:24:23 +0300

    Added functional buttons and screen for the game

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Руслан Мартынов on 21.03.2020.
+//  Copyright © 2020 Руслан Мартынов. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 
+public var changeButton = 0 // if 1 -> PlayWithComputer, if 2 -> PlayWithFriend
+
 class ViewController: UIViewController {
 // Publish storybord elements
-@IBOutlet weak var buttonPlayWithComputer: UIButton! // First button (Play with Computer)
-    
-@IBOutlet weak var buttonPlayWithFriend: UIButton! // Second button (Play with Friend)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        buttonPlayWithComputer.layer.cornerRadius = 10 // rounding button Play with Computer
-        buttonPlayWithFriend.layer.cornerRadius = 10 // rounding button Play with Computer
+        playWithComputer.layer.cornerRadius = 20 //rounding button
+        playWithFriend.layer.cornerRadius = 20 // rounding button
+    }
+    // MARK: - Button Functionality
+    
+    @IBOutlet weak var playWithComputer: UIButton!
+    @IBOutlet weak var playWithFriend: UIButton!
+    // First button (Play with Computer)
+    @IBAction func buttonPlayWithComputer(_ sender: Any) {
+        changeButton = 1
+        performSegue(withIdentifier: "StartGame", sender: self)
+    }
+    // Second button (Play with Friend)
+    @IBAction func buttonPlayWithFriend(_ sender: Any) {
+        changeButton = 2
+        performSegue(withIdentifier: "StartGame", sender: self)
     }
     
-
 }
-
